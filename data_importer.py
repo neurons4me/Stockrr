6f67f8ee2d99fdf150d4c71c268f7a74ee03dbac
Adam McDaniel <adammcdaniel1@gmail.com>
2018-10-08 15:34:35 -0700

    Add files via upload

--- data_importer.py
+++ data_importer.py
@@ -1,12 +1,17 @@
 import requests
-import json
-url_part_one = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
-url_part_two='&apikey='
-API_key = 'EQUA0WMPJLI4A9SQ'
 
-
-def request_builder(symbol):
+def request_time_series_daily(symbol):
+    url_part_one = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
+    url_part_two = '&apikey='
+    API_key = 'EQUA0WMPJLI4A9SQ'
     url = str(url_part_one) + str(symbol) + str(url_part_two) + str(API_key)
     r = requests.get(url)
     return r.json()
 
+def request_time_series_daily(symbol):
+    url_part_one = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
+    url_part_two = '&apikey='
+    API_key = 'EQUA0WMPJLI4A9SQ'
+    url = str(url_part_one) + str(symbol) + str(url_part_two) + str(API_key)
+    r = requests.get(url)
+    return r.json()
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import data_exporter
 import datetime
 import time
 
+
+
 #ticker = input('Enter Ticker Symbol')
 #do some inputr validation here
 
@@ -10,7 +12,7 @@ import time
 ticker_list = ['AAPL', 'GOOG', 'OGEN', 'CHK','TWTR', 'IGC']
 
 def volatility(ticker):
-    historical_data = data_importer.request_builder(str(ticker))
+    historical_data = data_importer.request_time_series_daily(str(ticker))
     historical_data = (historical_data['Time Series (Daily)'])
     output_file = {'symbol':ticker, 'report_date': str(datetime.date.today()), 'report_type': 'volatility', 'field_one_name': 'Date', 'field_two_name': 'Volatility % (Daily mean / Day High - Day Low)'}
     for day in historical_data:
@@ -24,9 +26,29 @@ def volatility(ticker):
         output_file[day] = variance_percent
     return output_file
 
-#TODO put inside a fucntion, add logging and error handling
-for symbol in ticker_list:
-    data_exporter.to_CSV(volatility(symbol))
-    time.sleep(15)
+#TODO add logging and error handling
+def fetch_volatility():
+    for symbol in ticker_list:
+        data_exporter.to_CSV(volatility(symbol))
+        time.sleep(15)
 
 
+
+#main menu
+exit_loop = False
+
+print('1. Modify Watchlist \n'
+      '2. Fetch Data \n'
+      '3. Other?')
+
+while exit_loop == False:
+    input_main_menu = input("Select an action by number \n")
+
+    if int(input_main_menu) > 3 or int(input_main_menu) < 1:
+        input_main_menu = input("Not a valid choice, please re-enter")
+    if int(input_main_menu) == 1:
+        print('run function to modify list')
+    if int(input_main_menu) == 2:
+        fetch_volatility()
+    if int(input_main_menu) == 3:
+        print("some other action")
