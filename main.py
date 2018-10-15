9b8d375125e200bb3bcbcc0a5f0b4b597abaf515
neurons4me <adammcdaniel1@gmail.com>
2018-10-14 22:16:46 -0700

    Some testing, error catching

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 import data_importer
 import data_exporter
-import datetime
 import time
-import collections
 
 
 
-#ticker = input('Enter Ticker Symbol')
-#do some inputr validation here
 
-#TODO make an import function to import a list from file to perfrom a batch
-ticker_list = ['AAPL', 'GOOG', 'OGEN', 'CHK','TWTR', 'IGC']
 
-# def volatility(ticker):
-#     historical_data = data_importer.request_time_series_daily(str(ticker))
-#     historical_data = (historical_data['Time Series (Daily)'])
-#     output_file = {'symbol':ticker, 'report_date': str(datetime.date.today()), 'report_type': 'volatility', 'field_one_name': 'Date', 'field_two_name': 'Volatility % (Daily mean / Day High - Day Low)'}
-#     for day in historical_data:
-#         day_data = historical_data[day]
-#
-#         variance_range = float(day_data['2. high']) - float(day_data['3. low'])
-#         variance_average =  (float(day_data['2. high']) + float(day_data['3. low'])) / 2
-#         variance_percent = (variance_range / variance_average) *100
-#
-#
-#         output_file[day] = variance_percent
-#     return output_file
 
 #TODO add logging and error handling
-def fetch_volatility():
-    for symbol in ticker_list:
-        data_exporter.to_CSV(volatility(symbol))
-        time.sleep(15)
 
 def RSI_checker(ticker):
     # 0 = overbought
@@ -82,30 +58,15 @@ def MOM_is_positive(ticker):
 
 
 def check_watchlist():
+    counter = 0
     for stock in data_importer.import_watchlist():
         if MOM_is_positive(stock) and RSI_checker(stock) == 2:
             print('{} {}'.format(stock, ' is a candidate!'))
-        #else:
-            #print('{}{}'.format(stock , ' is not a candidate.'))
+        counter += 1
+        if counter % 25 == 0:
+            print('{} {}'.format(counter, ' stocks have been checked.'))
         time.sleep(25)
 
 
 check_watchlist()
-# #main menu
-# exit_loop = False
-#
-# print('1. Modify Watchlist \n'
-#       '2. Fetch Data \n'
-#       '3. Other?')
-
-# while exit_loop == False:
-#     input_main_menu = input("Select an action by number \n")
-#
-#     if int(input_main_menu) > 3 or int(input_main_menu) < 1:
-#         input_main_menu = input("Not a valid choice, please re-enter")
-#     if int(input_main_menu) == 1:
-#         print('run function to modify list')
-#     if int(input_main_menu) == 2:
-#         fetch_volatility()
-#     if int(input_main_menu) == 3:
-#         print("some other action")
+#TODO doesnt seem to generate any positives, check to see if something is broken or if there really are no positives, suggest testing on a known positive ticker and checking the output at the MOM and RSI stages to see if that is working
