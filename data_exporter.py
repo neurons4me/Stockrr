bf8c13c18faec17c35ce3aac6ed7a8254cd1deae
Adam McDaniel <adammcdaniel1@gmail.com>
2018-10-16 16:00:47 -0700

    Add files via upload

--- data_exporter.py
+++ data_exporter.py
@@ -1,29 +1,37 @@
 import csv
+import datetime
 
 def name_builder(symbol, date, report_type):
     return str(symbol) + '_' + str(report_type) + '_' + date + '.csv'
 
 #takes a dictionary with symbol, report_type, report date, field_one_name, and field_two_name data as a header and any two other fields as main data and generates a simple report in csv format
-def to_CSV(report):
-    dictionary = report
-    symbol = dictionary['symbol']
-    report_type = dictionary['report_type']
-    report_date = dictionary['report_date']
-    file_name = str(symbol) + '_' + str(report_type) + '_' + report_date + '.csv'
-    field_one_name = dictionary['field_one_name']
-    field_two_name = dictionary['field_two_name']
-    del dictionary['symbol']
-    del dictionary['report_type']
-    del dictionary['report_date']
-    del dictionary['field_one_name']
-    del dictionary['field_two_name']
-    output_list = []
-    output_list.append([str(field_one_name) , str(field_two_name)])
-    for item in dictionary:
-        output_list.append([str(item), str(dictionary[item])])
+# def to_CSV(report):
+#     dictionary = report
+#     symbol = dictionary['symbol']
+#     report_type = dictionary['report_type']
+#     report_date = dictionary['report_date']
+#     file_name = str(symbol) + '_' + str(report_type) + '_' + report_date + '.csv'
+#     field_one_name = dictionary['field_one_name']
+#     field_two_name = dictionary['field_two_name']
+#     del dictionary['symbol']
+#     del dictionary['report_type']
+#     del dictionary['report_date']
+#     del dictionary['field_one_name']
+#     del dictionary['field_two_name']
+#     output_list = []
+#     output_list.append([str(field_one_name) , str(field_two_name)])
+#     for item in dictionary:
+#         output_list.append([str(item), str(dictionary[item])])
+#     with open(file_name, 'w+', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         for line in output_list:
+#             writer.writerow(line)
+
+def to_CSV(report_list, report_title):
+    report_date = str(datetime.date.today())
+    file_name = str(report_title) + '_' + report_date + '.csv'
     with open(file_name, 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        for line in output_list:
+        for line in report_list:
             writer.writerow(line)
-
 
--- main.py
+++ main.py
@@ -1,24 +1,54 @@
+import time
 import data_importer
 import data_exporter
-import time
+import datetime
 
+debug_mode = False
+RSI_codes = {
+    0 : "Overbought",
+    1 : "Neutral",
+    2 : "Oversold",
+    3 : "Data Error: Bad Header",
+    4 : "Data Error: Bad Data Body",
+    5 : "Data Error: API Call Rate Exceeded"
+}
+MOM_codes = {
+    0 : "Negative",
+    1 : "Neutral",
+    2 : "Positive",
+    3 : "Data Error: Bad Header",
+    4 : "Data Error: Bad Data Body",
+    5 : "Data Error: API Call Rate Exceeded"
+}
 
+RSI_trigger_oversold = 30
+RSI_trigger_overbought = 70
+sleep_time = 25
 
-
-
-
+#TODO clean up main.py... some of these functions make sense to belong to their own module
 #TODO add logging and error handling
+#TODO clean up according to PEP8
+#TODO add option of calling functions with custom settings and storing defaults in an easily accesable config file
+#TODO add CLI
+
 
 def RSI_checker(ticker):
+
+
+    # Return Code Key
     # 0 = overbought
     # 1 = neutral
     # 2 = oversold
-    historical_data = data_importer.request_RSI(str(ticker))
-    historical_data = (historical_data['Technical Analysis: RSI'])
-
-    #TODO tie to config file or global var
-    RSI_trigger_oversold = 30
-    RSI_trigger_overbought = 70
+    # 3 = Data Error: Bad Header
+    # 4 = Data Error: Bad Data Body
+    # 5 = Data Error: API Call Rate Exceeded
+    try:
+        historical_data = data_importer.request_RSI(str(ticker))
+        if historical_data == {"Information": "Thank you for using Alpha Vantage! Please visit https://www.alphavantage.co/premium/ if you would like to have a higher API call volume."}:
+            return 5
+        historical_data = (historical_data['Technical Analysis: RSI'])
+    except KeyError:
+        return 3
     output_file= []
     working_file = {}
     counter = 1
@@ -27,6 +57,8 @@ def RSI_checker(ticker):
         output_file.append(working_file[counter])
         counter += 1
     try:
+        if debug_mode:
+            print(output_file[1]['RSI'])
         if float(output_file[1]['RSI']) <= RSI_trigger_oversold:
             return 2
         elif float(output_file[1]['RSI']) >= RSI_trigger_overbought:
@@ -34,13 +66,27 @@ def RSI_checker(ticker):
         else:
             return 1
     except IndexError:
-        return 1
+        return 4
 
 
-def MOM_is_positive(ticker):
+def MOM_checker(ticker):
 
-    historical_data = data_importer.request_MOM(str(ticker))
-    historical_data = (historical_data['Technical Analysis: MOM'])
+
+    # Return Code Key
+    # 0 = Negative
+    # 1 = Neutral
+    # 2 = Positive
+    # 3 = Data Error: Bad Header
+    # 4 = Data Error: Bad Data Body
+    # 5 = Data Error: API Call Rate Exceeded
+    try:
+        # TODO refactor variable name... historical_data does not make much sense
+        historical_data = data_importer.request_MOM(str(ticker))
+        if historical_data == {"Information": "Thank you for using Alpha Vantage! Please visit https://www.alphavantage.co/premium/ if you would like to have a higher API call volume."}:
+            return 5
+        historical_data = (historical_data['Technical Analysis: MOM'])
+    except KeyError:
+        return 3
     output_file= []
     working_file = {}
     counter = 1
@@ -49,24 +95,45 @@ def MOM_is_positive(ticker):
         output_file.append(working_file[counter])
         counter += 1
     try:
-        if float(output_file[1]['MOM']) > 1:
-            return True
-        else:
-            return False
+        if debug_mode:
+            print(output_file[1]['MOM'])
+        if float(output_file[1]['MOM']) > 0:
+            return 2
+        elif float(output_file[1]['MOM']) < 0:
+            return 0
+        elif float(output_file[1]['MOM']) == 0:
+            return 1
     except IndexError:
-        return False
+        return 4
 
 
 def check_watchlist():
+    start_time = time.time()
     counter = 0
-    for stock in data_importer.import_watchlist():
-        if MOM_is_positive(stock) and RSI_checker(stock) == 2:
+    output_list = [["TICK", 'MOM', 'RSI']]
+    ticker_list = data_importer.import_watchlist()
+    estimate_time = round(len(ticker_list) * sleep_time / 60)
+    print('{} {}'.format(estimate_time, ' minutes expected runtime'))
+    for stock in ticker_list:
+        MOM_status = MOM_checker(stock)
+        RSI_status = RSI_checker(stock)
+        output_item = [stock, MOM_codes[MOM_status], RSI_codes[RSI_status]]
+        output_list.append(output_item)
+        if MOM_status and RSI_status == 2:
             print('{} {}'.format(stock, ' is a candidate!'))
+        else:
+            if debug_mode:
+                print('{} {} {} {} {}'.format(stock, 'MOM ', MOM_status, ' | RSI ', RSI_status))
         counter += 1
         if counter % 25 == 0:
             print('{} {}'.format(counter, ' stocks have been checked.'))
-        time.sleep(25)
+        time.sleep(sleep_time)
+    data_exporter.to_CSV(output_list, "RSI and Momentum Daily Checkup")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    elapsed_time_minutes = elapsed_time // 60
+    elapsed_time_seconds = round(elapsed_time % 60)
+    print('{} {} {} {} {} {} {}'.format('Processing completed on ', counter, ' equities in ', elapsed_time_minutes, ' minutes and ', elapsed_time_seconds, ' seconds.'))
 
 
 check_watchlist()
-#TODO doesnt seem to generate any positives, check to see if something is broken or if there really are no positives, suggest testing on a known positive ticker and checking the output at the MOM and RSI stages to see if that is working
